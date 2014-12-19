608cafa7c58c3c25157dae761ef80583e1c2f693
Gil Forsyth <gforsyth@gwu.edu>
2014-12-19 09:42:39 -0500

    lengthen workspace listing and add 日本語 support

--- workspace.sh
+++ workspace.sh
@@ -3,4 +3,4 @@
 while true ; do
     python2 dzen-workspaces.py
     sleep 1
-done | dzen2 -dock -x 850 -w 300 -fn '-*-clean-*-*-*-*-9-*-*-*-*-*-*-*' -xs "$1"
+done | dzen2 -dock -x 850 -w 500 -fn '-*-clean-*-*-*-*-9-*-*-*-*-*-*-*' -xs "$1"
