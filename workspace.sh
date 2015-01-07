f50557533f82f8e88b67a96032483f2e55e2fa0d
Gil Forsyth <gforsyth@gwu.edu>
2015-01-07 13:49:01 -0500

    add full path to python script

--- workspace.sh
+++ workspace.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 while true ; do
-    python2 dzen-workspaces.py
+    python2 ~/.dotfiles/dzen-workspaces.py
     sleep 1
 done | dzen2 -dock -x 850 -w 500 -fn '-*-clean-*-*-*-*-9-*-*-*-*-*-*-*' -xs "$1"
