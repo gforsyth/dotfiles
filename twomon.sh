73b841bb4864d615f2dcebceff8b3d7ee14dce48
Gil Forsyth <gforsyth@gwu.edu>
2015-01-22 10:42:00 -0500

    kill dzen everywhere then reinstantiate when adding monitor

--- twomon.sh
+++ twomon.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
 xrandr --output LVDS1 --auto --output HDMI1 --auto --left-of LVDS1
+kill $(pgrep -f ".workspaces.")
+kill $(pidof dzen2)
+/home/gil/.dotfiles/workspace.sh 1 &
+/home/gil/.dotfiles/workspace.sh 0 &
+/home/gil/.dotfiles/status.sh 1 &
+/home/gil/.dotfiles/status.sh 0 &
