d3ddfd5a89840a20f7a3efcedba404c8d0cfa4a2
Gil Forsyth <gforsyth@gwu.edu>
2015-01-22 10:42:20 -0500

    change spacing

--- workspace.sh
+++ workspace.sh
@@ -3,4 +3,4 @@
 while true ; do
     python2 ~/.dotfiles/dzen-workspaces.py
     sleep 1
-done | dzen2 -dock -x 850 -w 500 -fn '-*-clean-*-*-*-*-9-*-*-*-*-*-*-*' -xs "$1"
+done | dzen2 -dock -x 740 -w 400 -fn '-*-clean-*-*-*-*-9-*-*-*-*-*-*-*' -xs "$1"
