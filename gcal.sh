95f1ae9290d2a1830256b8419d541bccba735362
Gil Forsyth <gilforsyth@gmail.com>
2015-05-14 12:04:27 -0400

    show 4 weeks in calendar

--- gcal.sh
+++ gcal.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gcalcli --conky calw 3 --calendar gil --calendar "Holidays in United States" --nolineart 
+gcalcli --conky calw 4 --calendar gil --calendar "Holidays in United States" --nolineart 
