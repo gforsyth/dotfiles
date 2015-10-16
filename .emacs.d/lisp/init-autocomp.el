86b832c8233fdbd507efa58782d5f453071626ea
Gil Forsyth <gilforsyth@gmail.com>
2015-10-16 10:58:34 -0400

    remove insert-file-name function (already exists)
    dumbass.  just C-u C-c C-l

--- .emacs.d/lisp/init-autocomp.el
+++ .emacs.d/lisp/init-autocomp.el
@@ -2,29 +2,4 @@
              :ensure auto-complete)
 (global-auto-complete-mode t)
 
-;; function to enable insertion of filenames
-
- (defun my-insert-file-name (filename &optional args)
-    "Insert name of file FILENAME into buffer after point.
-  
-  Prefixed with \\[universal-argument], expand the file name to
-  its fully canocalized path.  See `expand-file-name'.
-  
-  Prefixed with \\[negative-argument], use relative path to file
-  name from current directory, `default-directory'.  See
-  `file-relative-name'.
-  
-  The default with no prefix is to insert the file name exactly as
-  it appears in the minibuffer prompt."
-    ;; Based on insert-file in Emacs -- ashawley 20080926
-    (interactive "*fInsert file name: \nP")
-    (cond ((eq '- args)
-           (insert (file-relative-name filename)))
-          ((not (null args))
-           (insert (expand-file-name filename)))
-          (t
-           (insert filename))))
-  
-  (global-set-key "\C-c\C-i" 'my-insert-file-name)
-
 (provide 'init-autocomp)
