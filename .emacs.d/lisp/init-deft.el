193c20b6d02666493338a2b7199798301f7fb66b
Gil Forsyth <gilforsyth@gmail.com>
2015-11-03 11:51:10 -0500

    update notes dir for deft (emacs)

--- .emacs.d/lisp/init-deft.el
+++ .emacs.d/lisp/init-deft.el
@@ -1,7 +1,7 @@
 ;; set up deft (note searching)
 (use-package deft
              :ensure deft)
-(setq deft-directory "~/Dropbox/notes")
+(setq deft-directory "~/notes")
 (setq deft-use-filename-as-title t)
 (setq deft-extension "org")
 (setq deft-text-mode 'org-mode)
