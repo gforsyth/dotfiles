bb67d2fcd95653e0640c58e1068cb5e9d782e429
Gil Forsyth <gil@forsyth.dev>
2020-12-23 16:15:12 -0500

    Add pdftools and set pdfs to open in right window

--- doom.d/config.el
+++ doom.d/config.el
@@ -11,3 +11,16 @@
 (setq doom-theme 'doom-city-lights)
 
 (setq shell-file-name "xonsh")
+
+
+(after! org
+  (add-to-list 'org-capture-templates
+             '("b" "Bill" table-line (file+headline "~/mnt/bills/bills.org" "Bills")
+               "| %? | %a | | |")))
+
+(add-to-list 'display-buffer-alist
+             '("\\.pdf\\(<[^>]+>\\)?$"
+               (display-buffer-in-side-window)
+               (side . right)
+               (window-width . 400)
+               ))
